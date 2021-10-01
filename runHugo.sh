1fdf9b1d798decf25239cec41cd9d5c162c74ca6
LiNk-NY <marcel.ramosperez@roswellpark.org>
2021-10-01 14:37:46 -0400

    update runHugo.sh file

--- runHugo.sh
+++ runHugo.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 ## NOTE publications are in 
-## ~/Documents/MyLib.bib
+## $HOME/professional/MyLib.bib
 
 ## my_site repository
-cd $HOME/gh/my_site
+cd $HOME/professional/my_site
+## make sure 
+## tpip3 install -U academic
 ## generate citations
-academic import --bibtex ~/Documents/MyLib.bib --overwrite 
+academic import --bibtex $HOME/professional/MyLib.bib --overwrite 
 ## make changes ... and view site
 hugo server
 ## build the site
-hugo -d $HOME/gh/LiNk-NY.github.io/
+hugo -d $HOME/professional/LiNk-NY.github.io/
 ## new contents are in the public/ folder
-cd $HOME/gh/LiNk-NY.github.io/
-
+cd $HOME/professional/LiNk-NY.github.io/
+## check for changes, commit, and push
 
